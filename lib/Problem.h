624436c2552d8150650aa2c4db073c0fb0719db3
terekj <30293049+terekj@users.noreply.github.com>
2025-11-03 18:09:16 -0800

    Update Problem

--- lib/Problem.h
+++ lib/Problem.h
@@ -3,30 +3,100 @@
 
 #include <vector>
 #include <utility>
+#include <ostream>
 
+const enum PuzzleOperation {MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT};
 
-struct Problem
-{
+struct Problem {
+	// function header
     std::vector<std::vector<char>> initial_state;
     std::vector<std::vector<char>> goal_state;
     std::vector<std::vector<char>> state;
+	int BLANK_TILE_X = -1;
+	int BLANK_TILE_Y = -1;
+	const int PUZZLE_SIZE = 3; // nxn puzzle
 
-    const std::vector<std::pair<int,int>> operators{
-        {-1, 0},//up
-        {1,0},//down
-        {0,1},//right
-        {-1,0}//left
-    };
+	friend std::ostream& operator<<(std::ostream& os, const Problem& obj);
 
+	void runOperation(PuzzleOperation operation);
+	void updateBlankTile();
+	std::vector<PuzzleOperation> getValidOperations();
+
+	// initializers
     Problem(std::vector<std::vector<char>> initial_state, std::vector<std::vector<char>> goal_state)
-    :initial_state{initial_state}, goal_state{goal_state} { }
+    :initial_state{initial_state}, goal_state{goal_state}, state(initial_state) { 
+		updateBlankTile();
+	}
 
 
-    void operator=(Problem& otherProblem)
+	// methods
+    void operator=(const Problem& otherProblem)
     {
         state = otherProblem.state;
     }
+	bool isGoalState() {
+		return (state == goal_state);
+	}
 
+	void updateBlankTile() {
+		for(int i = 0; i < PUZZLE_SIZE; ++i) {
+        	for (int j = 0; j < PUZZLE_SIZE; ++j) {
+				if(state[i][j] == ' ') {
+					BLANK_TILE_X = i;
+					BLANK_TILE_Y = j;
+				}
+			}
+    }
+	}
+	void runOperation(PuzzleOperation operation) {
+		std::vector<PuzzleOperation> validOperations = getValidOperations();
+		for (int i = 0; i < validOperations.size(); i++) {
+			if (validOperations.at(i) == operation) {
+				switch (operation) {
+					case MOVE_UP:
+						BLANK_TILE_Y--;
+						break;
+					case MOVE_DOWN:
+						BLANK_TILE_Y++;
+						break;
+					case MOVE_LEFT:
+						BLANK_TILE_X--;
+						break;
+					case MOVE_RIGHT:
+						BLANK_TILE_Y++;
+						break;
+					default:
+						break;
+				} 
+			}
+		}
+	}
+	std::vector<PuzzleOperation> getValidOperations() {
+		std::vector<PuzzleOperation> validOperations = {};
+		if (BLANK_TILE_Y > 0) {
+			validOperations.push_back(MOVE_UP);
+		}
+		if (BLANK_TILE_Y < PUZZLE_SIZE - 1) {
+			validOperations.push_back(MOVE_DOWN);
+		}
+		if (BLANK_TILE_X > 0) {
+			validOperations.push_back(MOVE_LEFT);
+		}
+		if (BLANK_TILE_X < PUZZLE_SIZE - 1) {
+			validOperations.push_back(MOVE_RIGHT);
+		}
+
+
+		return {};
+	}
 };
-
+std::ostream& operator<<(std::ostream& os, const Problem& obj) {
+	for(int i = 0; i < obj.PUZZLE_SIZE; ++i) {
+        for (int j = 0; j < obj.PUZZLE_SIZE; ++j) {
+			os << obj.state[i][j] << " ";
+		}
+		os << "\n";
+    }
+	return os; // Return the ostream reference for chaining
+}
 #endif //PROBLEM_H
